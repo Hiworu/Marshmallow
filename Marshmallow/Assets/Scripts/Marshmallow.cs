cd8f9771aebc788d317004c51fbc5f75ca2fc510
Elia Folisi <eliafolisi@gmail.com>
2025-01-15 19:14:22 +0100

    elia scureggiaforte

--- Marshmallow/Assets/Scripts/Marshmallow.cs
+++ Marshmallow/Assets/Scripts/Marshmallow.cs
@@ -5,22 +5,28 @@ using UnityEngine.UI;
 
 public class Marshmallow : MonoBehaviour
 {
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform jumpPoint;
     Rigidbody2D rb;
     [SerializeField] private List<GameObject> sudicio;
-    //[SerializeField] private Scrollbar jumpbar;
+    [SerializeField] private Image jumpbar;
     [SerializeField] private float PhaseTime;
-    private Dictionary<KeyCode, bool> keys = new Dictionary<KeyCode, bool>();
+    //private Dictionary<KeyCode, bool> keys = new Dictionary<KeyCode, bool>();
     private int phase;
     public float speed;
     private float acceleration;
     private float horizontal;
     public float jumpTimeCounter;
     public float jumpTime;
+    public float jumpForce;
+    public float TBarra = 5f;
     public bool isJumping;
     private bool isInLvl;
-    public float jumpForce;
-
-    GameObject player;
+    public bool perfJump;
+    public bool midJump;
+    public bool jumpPhase = true;
+    public float holdTimer;
+    public bool isHolding;
 
     void Start()
     {
@@ -35,7 +41,7 @@ public class Marshmallow : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space) && isJumping == false && isInLvl == false)
+        /*if(Input.GetKey(KeyCode.Space) && isJumping == false && isInLvl == false)
         {
            Debug.Log("JUMP BITCHHH");
            if(jumpTimeCounter > 0)
@@ -51,7 +57,50 @@ public class Marshmallow : MonoBehaviour
         if(Input.GetKeyUp(KeyCode.Space))
         {
            isJumping = false;
+        }*/
+
+        if(jumpPhase == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            isHolding = true;
+            StartCoroutine(Stanghett());
+            JumpPhase();
         }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isHolding = false;
+        }
+
+        if(isHolding == true)
+        {
+            holdTimer += Time.deltaTime;
+            jumpbar.fillAmount = holdTimer / TBarra;
+        }
+        else
+        {
+            jumpbar.fillAmount = 0;
+        }
+
+    }
+
+    private void JumpPhase()
+    {
+        if(perfJump == true)
+        {
+            jumpForce = 10;
+            player.transform. position = Vector3.MoveTowards(player.position, jumpPoint.position, speed);
+        }
+        else if(midJump == true)
+        {
+            jumpForce = 5;
+            player.transform. position = Vector3.MoveTowards(player.position, jumpPoint.position, speed);
+        }
+        else
+        {
+            jumpForce = 3;
+            player.transform. position = Vector3.MoveTowards(player.position, jumpPoint.position, speed);
+        }
+
+        jumpPhase = false;
     }
 
     private void OnTriggerEnter2D(Collider2D start)
@@ -76,6 +125,28 @@ public class Marshmallow : MonoBehaviour
             yield return null;
         }
         phase++;
+    }
+
+    private IEnumerator Stanghett()
+    {
+        while(holdTimer < TBarra)
+        {          
+            if(Input.GetKeyUp(KeyCode.Space) && holdTimer >= 3 && holdTimer < 4)
+            {
+                perfJump = true;
+                Debug.Log("perfect");
+            }
+            else if (Input.GetKeyUp(KeyCode.Space) && holdTimer >= 4 && holdTimer < 5)
+            {
+                midJump = true;
+                Debug.Log("troppo");
+            }
+            else if (Input.GetKeyUp(KeyCode.Space) && holdTimer < 3 && holdTimer > 0)
+            {
+                Debug.Log("meh");
+            }
+            yield return null;
+        }
     }
 
 }
